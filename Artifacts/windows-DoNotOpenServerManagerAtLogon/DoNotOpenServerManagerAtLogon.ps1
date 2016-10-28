bc83068910255377fdce9ec4edd2596e0d4b93f8
Altux <demalex17@gmail.com>
2016-10-29 00:17:40 +0200

    Add files via upload

---
+++ Artifacts/windows-DoNotOpenServerManagerAtLogon/DoNotOpenServerManagerAtLogon.ps1
@@ -0,0 +1,5 @@
+﻿#Do Not Open Server Manager At Logon
+
+$ServerManager ="HKCU:\Software\Microsoft\ServerManager"
+
+Set-itemProperty -Path $ServerManager -Name "DoNotOpenServerManagerAtLogon" -Value 1
