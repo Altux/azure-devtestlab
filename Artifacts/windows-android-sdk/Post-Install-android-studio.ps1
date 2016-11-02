918b970573fe94fe15f166fe45739730b775c1d8
Altux <demalex17@gmail.com>
2016-11-02 23:44:59 +0100

    Update Post-Install-android-studio.ps1

--- Artifacts/windows-android-sdk/Post-Install-android-studio.ps1
+++ Artifacts/windows-android-sdk/Post-Install-android-studio.ps1
@@ -1,2 +1,12 @@
-echo y | C:\Users\ArtifactInstaller\AppData\Local\Android\Android-sdk\tools\android.bat update sdk --no-ui --all --filter platform-tools,build-tools-24.0.1,android-22,android-23,doc-23,sys-img-armeabi-v7a-android-tv-23,sys-img-x86-android-tv-23,sys-img-armeabi-v7a-android-wear-23,sys-img-x86-android-wear-23,sys-img-armeabi-v7a-android-23,sys-img-x86_64-android-23,sys-img-x86-android-23,sys-img-armeabi-v7a-google_apis-23,sys-img-x86_64-google_apis-23,sys-img-x86-google_apis-23,sys-img-armeabi-v7a-android-tv-22,sys-img-x86-android-tv-22,sys-img-armeabi-v7a-android-wear-22,sys-img-x86-android-wear-22,sys-img-armeabi-v7a-android-22,sys-img-x86_64-android-22,sys-img-x86-android-22,sys-img-armeabi-v7a-google_apis-22,sys-img-x86_64-google_apis-22,sys-img-x86-google_apis-22,addon-google_apis-google-23,addon-google_apis-google-22,source-23,source-22,extra-google-m2repository,extra-google-usb_driver
-xcopy C:\Users\ArtifactInstaller\AppData\Local\Android\Android-sdk C:\Users\Administrateur\AppData\Local\Android
+#Download that file and install it in the right place
+
+$url = "https://dl.google.com/android/installer_r24.4.1-windows.exe"
+$path = C:\Packages\Scripts\installer_r24.4.1-windows.exe
+
+New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
+$client = new-object System.Net.WebClient 
+$client.DownloadFile($url, $path)
+
+C:\Packages\Scripts\installer_r24.4.1-windows.exe /S /D=C:\Users\Administrateur\AppData\Android
+
+echo y | C:\Users\Administrateur\AppData\Local\Android\Android-sdk\tools\android.bat update sdk --no-ui --all --filter platform-tools,build-tools-24.0.1,android-22,android-23,doc-23,sys-img-armeabi-v7a-android-tv-23,sys-img-x86-android-tv-23,sys-img-armeabi-v7a-android-wear-23,sys-img-x86-android-wear-23,sys-img-armeabi-v7a-android-23,sys-img-x86_64-android-23,sys-img-x86-android-23,sys-img-armeabi-v7a-google_apis-23,sys-img-x86_64-google_apis-23,sys-img-x86-google_apis-23,sys-img-armeabi-v7a-android-tv-22,sys-img-x86-android-tv-22,sys-img-armeabi-v7a-android-wear-22,sys-img-x86-android-wear-22,sys-img-armeabi-v7a-android-22,sys-img-x86_64-android-22,sys-img-x86-android-22,sys-img-armeabi-v7a-google_apis-22,sys-img-x86_64-google_apis-22,sys-img-x86-google_apis-22,addon-google_apis-google-23,addon-google_apis-google-22,source-23,source-22,extra-google-m2repository,extra-google-usb_driver
