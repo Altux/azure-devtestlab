12a3b4f1f290182f13cf2662f68fcd582bda6b84
Altux <demalex17@gmail.com>
2016-11-14 16:13:21 +0100

    Update Set-StartLayout-ESCP.ps1

--- Artifacts/Windows-Upload-script-start-layout/Set-StartLayout-ESCP.ps1
+++ Artifacts/Windows-Upload-script-start-layout/Set-StartLayout-ESCP.ps1
@@ -12,8 +12,22 @@ function Pin-App { param(
     ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $App}).verbs() | ?{$_.Name.replace('&','') -match "Pin to Start"} | %{$_.DoIt()}
 }
 
+
+
+function DownloadFIle($url, $path)
+{
+    New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
+    $client = new-object System.Net.WebClient 
+    $client.DownloadFile($url, $path)
+}
+
+
 mkdir "C:\Users\Administrateur\AppData\Local\Android\Sdk"
 move "C:\Users\artifactInstaller\AppData\Local\Android\android-sdk\*" "C:\Users\Administrateur\AppData\Local\Android\Sdk"
+
+
+C:\Users\Administrateur\AppData\Local\Android\Sdk\tools\android.bat create avd -t "android-10" -b "armeabi" -n "ESCP-V3_API_10"
+DownloadFile("https://raw.githubusercontent.com/Altux/azure-devtestlab/master/Applications%20Config%20File/Android%20Studio/config.ini", "C:\Users\Administrateur\.android\avd\ESCP-V3_API_10.avd")
 
 Start-Sleep -s 45
 
