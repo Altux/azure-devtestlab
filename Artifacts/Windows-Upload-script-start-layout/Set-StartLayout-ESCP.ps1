9e6453ca05eebf27d8f19e77cf62c7199085b42f
Altux <demalex17@gmail.com>
2016-11-14 17:50:42 +0100

    Update Set-StartLayout-ESCP.ps1

--- Artifacts/Windows-Upload-script-start-layout/Set-StartLayout-ESCP.ps1
+++ Artifacts/Windows-Upload-script-start-layout/Set-StartLayout-ESCP.ps1
@@ -12,25 +12,23 @@ function Pin-App { param(
     ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $App}).verbs() | ?{$_.Name.replace('&','') -match "Pin to Start"} | %{$_.DoIt()}
 }
 
-
-
-function DownloadFIle($url, $path)
-{
-    New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
-    $client = new-object System.Net.WebClient 
-    $client.DownloadFile($url, $path)
-}
-
-
+#Move SDK Folder to the Administrateur Profile
 mkdir "C:\Users\Administrateur\AppData\Local\Android\Sdk"
 move "C:\Users\artifactInstaller\AppData\Local\Android\android-sdk\*" "C:\Users\Administrateur\AppData\Local\Android\Sdk"
 
-
+#Creation of a AVD
 C:\Users\Administrateur\AppData\Local\Android\Sdk\tools\android.bat create avd -t "android-10" -b "armeabi" -n "ESCP-V3_API_10" -d "4in WVGA (Nexus S)"
-DownloadFile("https://raw.githubusercontent.com/Altux/azure-devtestlab/master/Applications%20Config%20File/Android%20Studio/config.ini", "C:\Users\Administrateur\.android\avd\ESCP-V3_API_10.avd")
+
+#Download the config.ini
+$url = "https://raw.githubusercontent.com/Altux/azure-devtestlab/master/Applications%20Config%20File/Android%20Studio/config.ini"
+$path = "C:\Users\Administrateur\.android\avd\ESCP-V3_API_10.avd\config.ini"
+New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
+$client = new-object System.Net.WebClient 
+$client.DownloadFile($url, $path)
 
 Start-Sleep -s 45
 
+#Create the Start-Layout
 Unpin-App "Server Manager"
 Unpin-App "Windows Powershell"
 Unpin-App "Windows Powershell ISE"
