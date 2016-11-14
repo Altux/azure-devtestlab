be35d8c26032b206a19a189b2476c2a7df09fdbf
Altux <demalex17@gmail.com>
2016-11-14 16:09:12 +0100

    Update Post-Install-android-studio.ps1

--- Artifacts/windows-android-sdk/Post-Install-android-studio.ps1
+++ Artifacts/windows-android-sdk/Post-Install-android-studio.ps1
@@ -1,25 +1,13 @@
 #Download that file and install it in the right place
 
-$url1 = "https://dl.google.com/android/installer_r24.4.1-windows.exe"
-$path1 = "C:\Packages\Scripts\installer_r24.4.1-windows.exe"
+$url = "https://dl.google.com/android/installer_r24.4.1-windows.exe"
+$path = "C:\Packages\Scripts\installer_r24.4.1-windows.exe"
 
-$url2 = "https://raw.githubusercontent.com/Altux/azure-devtestlab/master/Applications%20Config%20File/Android%20Studio/config.ini"
-$path2 = "C:\Users\Administrateur\.android\avd\ESCP-V3_API_10.avd"
-
-
-function DownloadFIle($url, $path)
-{
-    New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
-    $client = new-object System.Net.WebClient 
-    $client.DownloadFile($url, $path)
-}
-
-DownloadFile($url1, $path1)
+New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
+$client = new-object System.Net.WebClient 
+$client.DownloadFile($url, $path)
 
 C:\Packages\Scripts\installer_r24.4.1-windows.exe /S
 start-sleep 300
 
 echo y | C:\Users\ArtifactInstaller\AppData\Local\Android\Android-sdk\tools\android.bat update sdk --no-ui
-C:\Users\ArtifactInstaller\AppData\Local\Android\Android-sdk\tools\android.bat create avd -t "android-10" -b "armeabi" -n "ESCP-V3_API_10"
-
-DownloadFile($url2, $path2)
