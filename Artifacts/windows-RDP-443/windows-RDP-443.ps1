ae35d9c3cadb387aa7b062a24430ac363a9f2ad3
Altux <demalex17@gmail.com>
2016-11-15 18:06:22 +0100

    Update windows-RDP-443.ps1

--- Artifacts/windows-RDP-443/windows-RDP-443.ps1
+++ Artifacts/windows-RDP-443/windows-RDP-443.ps1
@@ -5,3 +5,5 @@ Set-itemProperty -Path $RDP -Name "PortNumber" -Value 443
 #Set New FIrewall Rules to let 443 port on RDP
 New-NetFirewallRule -DisplayName "Allow 443 on RDP" -Direction "Inbound" -Program "%SystemRoot%\system32\svchost.exe" -Protocol TCP -Localport "443" -action allow
 
+net stop termservice
+net start termservice
