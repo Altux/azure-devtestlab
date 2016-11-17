d455892e1c74b9d685d12bf0b8c0f71fecda76e6
Altux <demalex17@gmail.com>
2016-11-17 18:35:46 +0100

    Update shutdown-download.ps1

--- Artifacts/windows-shutdown-popup/shutdown-download.ps1
+++ Artifacts/windows-shutdown-popup/shutdown-download.ps1
@@ -5,8 +5,10 @@ New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)
 $client = new-object System.Net.WebClient 
 $client.DownloadFile($url, $path)
 
+$U = "Administrateur"
 $A = New-ScheduledTaskAction -Execute "powershell.exe" -argument "C:\Packages\Scripts\windows-shutdown-popup.ps1"
 $T = New-ScheduledTaskTrigger -AtLogon
-$P = "Administrateur"
-$D = New-ScheduledTask -Action $A -Trigger $T -Principal $P
-Register-ScheduledTask T1 -InputObject $D
+$D = New-ScheduledTask -Action $A -Trigger $T
+Register-ScheduledTask T1 -InputObject $D -User $U
+
+
