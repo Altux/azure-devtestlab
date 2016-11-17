aa744915f4890a41c92135c958618d26703ca9c4
Altux <demalex17@gmail.com>
2016-11-17 17:47:29 +0100

    Update shutdown-download.ps1

--- Artifacts/windows-shutdown-popup/shutdown-download.ps1
+++ Artifacts/windows-shutdown-popup/shutdown-download.ps1
@@ -1,11 +1,11 @@
-$url = "" 
+$url = "https://raw.githubusercontent.com/Altux/azure-devtestlab/master/Artifacts/windows-shutdown-popup/windows-shutdown-popup.ps1" 
 $path = "C:\Packages\Scripts\windows-shutdown-popup.ps1"
 
 New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
 $client = new-object System.Net.WebClient 
 $client.DownloadFile($url, $path)
 
-$A = New-ScheduledTaskAction –Execute "powershell.exe" -argument "C:\Packages\Scripts\windows-shutdown-popup.ps1"
+$A = New-ScheduledTaskAction â€“Execute "powershell.exe" -argument "C:\Packages\Scripts\windows-shutdown-popup.ps1"
 $T = New-ScheduledTaskTrigger -AtLogon
 $D = New-ScheduledTask -Action $A -Trigger $T
 Register-ScheduledTask T1 -InputObject $D
