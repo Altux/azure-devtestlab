9f48e73f316f343ac45abe2b843335949d21df9d
Altux <demalex17@gmail.com>
2016-11-18 12:52:40 +0000

    Add files via upload

---
+++ Artifacts/Windows-Chrome-Extention/ChromeInstallExtention.ps1
@@ -0,0 +1,4 @@
+$EXTENTIONID = "gbchcmhmhahfdphkhkmpfmihenigjmpp"
+
+New-Item -Path "HKLM:\Software\Wow6432Node\Google\Chrome\Extensions" -Name $EXTENTIONID
+Set-ItemProperty -Path "HKLM:\Software\Wow6432Node\Google\Chrome\Extensions\$EXTENTIONID" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
