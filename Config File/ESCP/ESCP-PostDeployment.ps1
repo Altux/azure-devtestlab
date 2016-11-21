c4694299c002a815b27ad9c65eb3808cd8a1f03b
Altux <demalex17@gmail.com>
2016-11-21 15:50:44 +0000

    Add files via upload

---
+++ Config File/ESCP/ESCP-PostDeployment.ps1
@@ -0,0 +1,108 @@
+#
+# Powershell Configurations
+#
+
+# Note: Because the $ErrorActionPreference is "Stop", this script will stop on first failure. 
+$ErrorActionPreference = "stop"
+
+# Ensure that current process can run scripts. 
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
+
+###################################################################################################
+
+#
+# Functions
+#
+
+function DesktopShortcut ($name,$target)
+{
+	write-host "Creating $name Shortcut to the Desktop..."
+	$WshShell = New-Object -comObject WScript.Shell
+	$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\$name.lnk")
+	$Shortcut.TargetPath = $target
+	$Shortcut.Save()
+}
+
+function Unpin-App 
+{ 
+	param([string]$App)
+	((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $App}).verbs() | ?{$_.Name.replace('&','') -match "Unpin from Start"} | %{$_.DoIt()}
+	write-host "Remove $app to the start-menu ..."
+}
+
+function Pin-App 
+{ 
+	param([string]$App)
+    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $App}).verbs() | ?{$_.Name.replace('&','') -match "Pin to Start"} | %{$_.DoIt()}
+	write-host "Adding $app to the start-menu ..."
+}
+
+function SetupAndroidStudio{
+	Write-host "Setup of the Android Studio"
+
+	#Creation of a AVD
+		C:\Users\Administrateur\AppData\Local\Android\Sdk\tools\android.bat create avd -t "android-10" -b "armeabi" -n "ESCP-V3_API_10" -d "4in WVGA (Nexus S)"
+
+	#Download the config.ini
+		$url = "https://raw.githubusercontent.com/Altux/azure-devtestlab/master/Applications%20Config%20File/Android%20Studio/config.ini"
+		$path = "C:\Users\Administrateur\.android\avd\ESCP-V3_API_10.avd\config.ini"
+		New-Item -ItemType Directory -Force -Path (Split-Path -parent $path)    
+		$client = new-object System.Net.WebClient 
+		$client.DownloadFile($url, $path)
+
+
+	#Add Project Android Studio Shortcut to the desktop
+		$WshShell = New-Object -comObject WScript.Shell
+		$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\AndroidStudioProjects.lnk")
+		$Shortcut.TargetPath = "C:\Windows\explorer.exe"
+		$Shortcut.Arguments ="C:\Users\Administrateur\AndroidStudioProjects"
+		$Shortcut.Save()
+
+}
+
+
+
+##################################################################################################
+
+try
+{
+	#Setup Android Studio
+	SetupAndroidStudio
+
+	#Add Shorcut to the desktop
+	DesktopShortcut ("Google Drive", "C:\Program Files (x86)\Google\Drive\googledrivesync.exe")
+	DesktopShortcut ("Android Studio", "C:\Program Files\Android\Android Studio\bin\studio64.exe")
+	DesktopShortcut ("GoogleDrive", "C:\Program Files (x86)\Google\Drive\googledrivesync.exe")
+	DesktopShortcut ("GoogleDrive", "C:\Program Files (x86)\Google\Drive\googledrivesync.exe")
+	
+	#Change Hours
+	C:\Windows\system32\tzutil /s "Romance Standard Time"
+	
+	Start-Sleep -s 45
+	
+	#Unpin all tiles
+	Unpin-App "Server Manager"
+	Unpin-App "Windows Powershell"
+	Unpin-App "Windows Powershell ISE"
+	Unpin-App "Windows Administrative tools"
+	Unpin-App "Task Manager"
+	Unpin-App "Control Panel"
+	Unpin-App "File Explorer"
+	Unpin-App "Event Viewer"
+	Unpin-App "Remote Desktop Connection"
+
+	#Pin tiles needed
+	Pin-App "Android Studio"
+	Pin-App "Google Drive"
+	Pin-App "File Explorer"
+	Pin-App "Google Chrome"
+	return 0
+}
+
+catch
+{
+	write-host "An Error Occured"
+    return -1
+
+}
+
