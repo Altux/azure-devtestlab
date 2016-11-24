f3e3a67c9acb0da3bb19e0d0119da329e5214d85
Alexandre Medeiros <demalex17@gmail.com>
2016-11-24 17:31:56 +0100

    d

--- Config File/ESCP/ESCP-PostDeployment.ps1
+++ Config File/ESCP/ESCP-PostDeployment.ps1
@@ -34,7 +34,7 @@
 #
 
 # Note: Because the $ErrorActionPreference is "Stop", this script will stop on first failure. 
-$ErrorActionPreference = "stop"
+#$ErrorActionPreference = "stop"
 
 # Ensure that current process can run scripts. 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
