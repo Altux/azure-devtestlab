d2c3ea9b6f1ac5e5706ad1dfc7619192dada9b07
Alexandre Medeiros <demalex17@gmail.com>
2017-03-20 08:30:13 +0100

    PATCH

---
+++ Artifacts/patch/Patch.ps1
@@ -0,0 +1,10 @@
+﻿rm c:\programdata\Exaduo\quota\*
+copy "./Windows-quota.ps1" "c:\programdata\Exaduo\quota\Windows-quota.ps1"
+
+$U = "SYSTEM"
+$A = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -argument "-windowstyle hidden -executionpolicy Unrestricted  C:\ProgramData\Exaduo\ShutdownLogOff\windows-shutdownlogoff.ps1"
+$T = New-ScheduledTaskTrigger -AtStartup 
+$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+$P = New-ScheduledTaskPrincipal -RunLevel Highest -UserId SYSTEM
+$D = New-ScheduledTask -Action $A -Trigger $T -Settings $S -Principal $P
+Register-ScheduledTask T2 -InputObject $D -User $U
