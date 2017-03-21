39bfb0975fb638ad9a149216d3c7af063d980805
Alexandre Medeiros <demalex17@gmail.com>
2017-03-21 10:54:59 +0100

    Correction BUG

--- Artifacts/windows-shutdownlogoff2/post.ps1
+++ Artifacts/windows-shutdownlogoff2/post.ps1
@@ -7,4 +7,4 @@ $T = New-ScheduledTaskTrigger -AtStartup
 $S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 $P = New-ScheduledTaskPrincipal -RunLevel Highest -UserId SYSTEM
 $D = New-ScheduledTask -Action $A -Trigger $T -Settings $S -Principal $P
-Register-ScheduledTask T3 -InputObject $D -User $U
+Register-ScheduledTask T2 -InputObject $D -User $U
